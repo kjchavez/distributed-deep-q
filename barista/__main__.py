cabf1d304ac04ca4fc332fafe41b946472142538
Kevin Chavez <kjchavez@stanford.edu>
2015-04-24 16:49:32 -0700

    Added __main__.py to module

--- barista/__main__.py
+++ barista/__main__.py
@@ -58,9 +58,7 @@ def main():
     parser.add_argument("--driver", default="127.0.0.1:5000")
     args = parser.parse_args()
 
-    print "PID:", os.getpid()
     caffe.set_phase_test()
-
     if args.mode == "cpu":
         caffe.set_mode_cpu()
     else:
