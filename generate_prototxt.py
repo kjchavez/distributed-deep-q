c4dcbdb9a72d85d9d0a82c07d5af0f5c8b85da8a
Kevin Chavez <kjchavez@stanford.edu>
2015-04-24 19:12:39 -0700

    Added script to automatically create prototxt for arbitrarily many actions / gamma value

---
+++ generate_prototxt.py
@@ -0,0 +1,29 @@
+# Create .prototxt from template
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("output")
+parser.add_argument("num_actions", type=int)
+parser.add_argument("--gamma", type=float, default=0.85)
+
+args = parser.parse_args()
+
+with open("models/deepq/train_val.template") as template:
+    with open(args.output, 'w') as prototxt:
+        for line in template:
+            line = line.rstrip()
+            if "<GAMMA>" in line:
+                newline = line.replace("<GAMMA>", str(args.gamma))
+                print >> prototxt, newline
+            elif "<NUM_ACTIONS>" in line:
+                newline = line.replace("<NUM_ACTIONS>", str(args.num_actions))
+                print >> prototxt, newline
+            elif "<ACTION_REPEAT> " in line:
+                newline = line.replace("<ACTION_REPEAT> ", '')
+                num_repeat = args.num_actions
+                if "slice_point" in newline:
+                    num_repeat = args.num_actions - 1
+                for i in xrange(num_repeat):
+                    print >> prototxt, newline % (i+1)
+            else:
+                print >> prototxt, line
