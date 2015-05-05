bbf182e13c338287c92bd2080d46fb487612994a
haoyio <haoyi@stanford.edu>
2015-05-05 11:15:23 -0700

    merged ExpGain from branch

--- exp-gain/ExpGain.py
+++ exp-gain/ExpGain.py
@@ -1,20 +1,50 @@
+import numpy as np
 import random
+from collections import deque
+from ..replay.dataset import ReplayDataset
+
+_FRAME_LIMIT = 1000000
+_EPSILON_MAX = 1.0
+_EPSILON_MIN = 0.1
+_NFRAME = 4
 
 class ExpGain(object):
-    def __init__(network, actions, preprocessor, game):
-        self.network = network
-        self.actions = actions
-        self.preprocessor = preprocessor
-        self.game = game
+    def __init__(self, network, actions, preprocessor, game, dataset, init_state):
+        self.network = network # dqn action selector
+        self.actions = actions # list of actions
+        self.preprocessor = preprocessor # downsampler
+        self.game = game # game updater
+        self.dataset = dataset # replay_dataset object
+        self.sequence = deque() # sequence of frames
+        for _ in range(_NFRAME):
+            self.sequence.append(init_state)
 
-    def select_action(pstate, epsilon):
+    def select_action(self, pstate, epsilon):
         if random.random() < epsilon:
             return random.choice(self.actions)
         else:
-            return network(pstate)
+            return self.network(pstate)
 
-    def generate_experience(state, epsilon):
-        pstate = self.preprocessor(state)
-        action = select_action(pstate, epsilon)
-        new_state, reward = self.game(state, action)
-        return (pstate, action, reward, self.preprocessor(new_state))
+    def get_epsilon(self, iter_num):
+        if iter_num > _FRAME_LIMIT:
+            return _EPSILON_MIN
+        else:
+            return _EPSILON_MIN + (_EPSILON_MAX - _EPSILON_MIN) \
+                   * iter_num / _FRAME_LIMIT
+
+    def arrayify_frames(self):
+        nx, ny = self.sequence[0].shape
+        array = np.zeros((nx, ny, _NFRAME), dtype=np.int)
+        for frame in range(_NFRAME):
+            array[:, :, frame] = self.sequence[frame]
+        return array
+
+    def generate_experience(self, iter_num):
+        pstate = self.preprocessor(self.arrayify_frames())
+        action = self.select_action(pstate, self.get_epsilon(iter_num))
+        new_state, reward = self.game(self.sequence[-1], action)
+        self.states.popleft()
+        self.states.append(new_state)
+        self.dataset.add_experience(
+            action, reward, self.preprocessor(self.arrayify_frames())
+        )
