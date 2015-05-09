580b8a38bca66c4c770b94129793bf910c4eac8e
auhong <auhong@stanford.edu>
2015-05-09 01:33:28 -0700

    fixing param server issue and adding end pointing for clearing stored model

--- param-server/server.py
+++ param-server/server.py
@@ -2,6 +2,9 @@ from flask import Flask, Response, jsonify, request
 from barista import messaging
 from redis import Redis
 import redis_collections as redisC
+import pdb
+
+
 app = Flask(__name__)
 
 
@@ -12,22 +15,29 @@ def hello():
 @app.route('/api/v1/latest_model', methods=['GET'])
 def get_model_params():
   # assuming the return message would be a string(of bytes)
-  # print centralModel['Qconv1'][0]
-  model = RedisC(key="centralModel")
+  model = redisC.Dict(key="centralModel")
+  print model['Qconv1'][0]
   m = messaging.create_message(dict(model), compress=False)
+  # pdb.set_trace()
   return Response(m, status=200)
 
 @app.route('/api/v1/update_model', methods=['POST'])
 def update_params():
   updateParams = messaging.load_gradient_message(request.data, compressed = False)
-  # print updateParams['Qconv1'][0]
+  print updateParams['Qconv1'][0]
   update(updateParams)
   return Response("Updated", status=200)
+
+@app.route('/api/v1/clear_model', methods=['POST'])
+def clear_params():
+  model = redisC.Dict(key="centralModel")
+  model.clear()
+  return Response("Cleared", status=200)
 
 def update(params):
   # get model stored in redis
   model = redisC.Dict(key="centralModel")
-  model.update(params)
+  model.update(d)
   return 
 
 def initParams():
