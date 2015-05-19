90fe7c8de13deb9eb63837e9506f154a7682385b
Kevin Chavez <kjchavez@stanford.edu>
2015-05-18 22:06:01 -0700

    Cleaned up updates in server.py

--- param-server/server.py
+++ param-server/server.py
@@ -1,36 +1,86 @@
 import argparse
 
-from flask import Flask, Response, jsonify, request
+from flask import Flask, Response, request
 from barista import messaging
 from redis import Redis
 import redis_collections as redisC
-import pdb
 import numpy as np
 
 from caffe import SGDSolver
 
-SGD_ALPHA = 0.01
+# Global settings, only set once, when the server is started
+step_size = 0.0
+rmsprop_decay = 0.9
+update_fn = None
+
 app = Flask(__name__)
 
 
-def add_to_model(model, updates, weight, scale=None):
+def apply_descent(model_name, updates, weight=1, scale=None, fn=lambda x: x):
     """ Applies an update to the model parameters.
 
     Args:
-        model:  dict-like container of all parameters
+        model_name: key for the model in the redis server
         updates: dict-like container of updates (must be subset of model)
         weight:  int or float by which the update is multiplied
         scale:   dict-like container of normalization factors by which to scale
                  individual elements of the updates (a la Adagrad or RMSProp)
+        fn:     function to apply to scale
     """
+    model = redisC.Dict(key="centralModel")
     if scale is None:
         for key in updates:
-            for i in range(len(updates[key])):
-                model[key][i] += weight*updates[key][i]
+            model[key] = [model[key][i] - weight*updates[key][i]
+                          for i in range(len(updates[key]))]
     else:
         for key in updates:
-            for i in range(len(updates[key])):
-                model[key][i] += weight*updates[key][i]/scale[key][i]
+            model[key] = [model[key][i] - weight*updates[key][i]/fn(scale[key][i])
+                          for i in range(len(updates[key]))]
+
+
+def sgd_update(updateParams):
+    print "[SGD UPDATE]"
+    apply_descent("centralModel", updateParams, weight=step_size)
+
+
+def rmsprop_update(updateParams):
+    print "[RMSPROP UPDATE]"
+    rmsprop = redisC.Dict(key="rmsprop")
+    if not rmsprop:
+        for k in updateParams:
+            params = []
+            for i in range(len(updateParams[k])):
+                params.append(updateParams[k][i]**2)
+            rmsprop[k] = params
+    else:
+        for k in updateParams:
+            rmsprop[k] = [rmsprop_decay * rmsprop[k][i] +
+                          (1-rmsprop_decay) * updateParams[k][i]**2
+                          for i in range(len(updateParams[k]))]
+
+    apply_descent("centralModel", updateParams,
+                  weight=step_size, scale=rmsprop,
+                  fn=lambda x: np.sqrt(x+1e-8))
+
+
+def adagrad_update(updateParams):
+    print "[ADAGRAD UPDATE]"
+    adagrad = redisC.Dict(key="adagrad")
+    if not adagrad:
+        for k in updateParams:
+            params = []
+            for i in range(len(updateParams[k])):
+                params.append(updateParams[k][i]**2)
+            adagrad[k] = params
+    else:
+        for k in updateParams:
+            adagrad[k] = [adagrad[k][i] + updateParams[k][i]**2
+                          for i in range(len(updateParams[k]))]
+
+    apply_descent("centralModel", updateParams,
+                  weight=step_size, scale=adagrad,
+                  fn=lambda x: np.sqrt(x+1e-8))
+
 
 @app.route("/")
 def hello():
@@ -47,37 +97,11 @@ def get_model_params():
     return Response(m, status=200)
 
 
-def rmsprop(updateParams):
-    rmsprop = redisC.Dict(key="rmsprop")
-    if not rmsprop:
-        rmsprop = updateParams
-    else:
-        for k in updateParams:
-            rmsprop[k] = 0.9 * rmsprop[k] + 0.1 * updateParams[k]**2
-
-
-def adagrad(updateParams):
-    adagrad = redisC.Dict(key="adagrad")
-    if not adagrad:
-        for k in updateParams:
-            params = []
-            for i in range(len(updateParams[k])):
-                params.append(updateParams[k][i]**2)
-            adagrad[k] = params
-    else:
-        for k in updateParams:
-            for i in range(len(updateParams[k])):
-                adagrad[k][i] += updateParams[k][i]**2
-
-
 @app.route('/api/v1/update_model', methods=['POST'])
 def update_params():
-    updateParams = messaging.load_gradient_message(request.data, compressed = False)
-
-    # rmsprop(updateParams)
-    adagrad(updateParams)
-
-    SGDUpdate(updateParams)
+    updateParams = messaging.load_gradient_message(request.data)
+    print "Grad. Qconv1 norm", np.linalg.norm(updateParams['Qconv1'][0])
+    update_fn(updateParams)
     return Response("Updated", status=200)
 
 
@@ -86,31 +110,6 @@ def clear_params():
     model = redisC.Dict(key="centralModel")
     model.clear()
     return Response("Cleared", status=200)
-
-
-def SGDUpdate(params):
-    # get model and rms_meansq stored in redis
-    model = redisC.Dict(key="centralModel")
-    # rmsprop = redisC.Dict(key="rmsprop")
-    adagrad = redisC.Dict(key="adagrad")
-    print adagrad.keys()
-    # print model
-    for k in params:
-        if k not in model:
-            print "Warning: parameter %s not found in model." % k
-            model[k] = []
-        for i in range(len(params[k])):
-            if len(model[k]) < (i+1):
-                arr = model[k]
-                arr.append(np.zeros(params[k][i].shape,
-                                    dtype=params[k][i].dtype))
-                model[k] = arr
-
-            # update model with rmsprop
-            # model[k][i] -= SGD_ALPHA * params[k][i] / np.sqrt(rmsprop[k][i])
-
-            # update model with adagrad
-            model[k][i] -= SGD_ALPHA * params[k][i] / np.sqrt(adagrad[k][i])
 
 
 def initParams(solver_filename, reset=True):
@@ -152,6 +151,10 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("solver")
     parser.add_argument("--reset", action="store_true")
+    parser.add_argument("--update", choices=['adagrad', 'rmsprop', 'sgd'],
+                        default='rmsprop')
+    parser.add_argument("--stepsize", type=float, default=1e-3)
+    parser.add_argument("--rmsprop_decay", type=float, default=0.9)
 
     args = parser.parse_args()
     return args
@@ -159,5 +162,16 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
+
+    # Initialize global settings
+    step_size = args.stepsize
+    if args.update == "sgd":
+        update_fn = sgd_update
+    elif args.update == "rmsprop":
+        update_fn = rmsprop_update
+        rmsprop_decay = args.rmsprop_decay
+    elif args.update == "adagrad":
+        update_fn = adagrad_update
+
     initParams(args.solver, reset=args.reset)
     app.run(debug=True, port=5500)
