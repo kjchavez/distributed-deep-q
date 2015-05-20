fa3c1c7ed9a38748e5b8c646abf476c1398b30c8
haoyio <haoyi@stanford.edu>
2015-05-20 11:44:19 -0700

    subclassed slider to discrete slider for frame idx

--- vizgame.py
+++ vizgame.py
@@ -40,20 +40,53 @@ def gen_image(frame, frame_number):
     misc.imsave("frame" + str(frame_number) + ".png", frame)
 
 
+class DiscreteSlider(Slider):
+    """ A matplotlib slider widget with discrete steps. """
+
+    def __init__(self, *args, **kwargs):
+        """Identical to Slider.__init__, except for the "increment" kwarg.
+        "increment" specifies the step size that the slider will be discritized
+        to."""
+        self.inc = kwargs.pop('increment', 0.5)
+        Slider.__init__(self, *args, **kwargs)
+
+    def set_val(self, val):
+        discrete_val = int(val / self.inc) * self.inc
+        # We can't just call Slider.set_val(self, discrete_val), because this
+        # will prevent the slider from updating properly (it will get stuck at
+        # the first step and not "slide"). Instead, we'll keep track of the
+        # the continuous value as self.val and pass in the discrete value to
+        # everything else.
+        xy = self.poly.xy
+        xy[2] = discrete_val, 1
+        xy[3] = discrete_val, 0
+        self.poly.xy = xy
+        self.valtext.set_text(self.valfmt % discrete_val)
+        if self.drawon:
+            self.ax.figure.canvas.draw()
+        self.val = val
+        if not self.eventson:
+            return
+        for cid, func in self.observers.iteritems():
+            func(discrete_val)
+
+
 def view_images(frames):
-    ''' |frames| is a list of numpy arrays. '''
+    """ |frames| is a list of numpy arrays. """
 
     fig, ax = plt.subplots()
     plt.subplots_adjust(left=0.25, bottom=0.25)
-    l, = plt.imshow(frames[0])
+    l = plt.imshow(frames[0], cmap=cm.Greys_r, interpolation="none")
 
     axcolor = 'lightgoldenrodyellow'
-    axframe = plt.axes(list(range(len(frames))), axisbg=axcolor)
-    sframe = Slider(axframe, "Frame no.", 0.1, 30.0, valinit=0)
+    axframe = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+    sframe = DiscreteSlider(axframe, "Frame no.", 0, len(frames) - 1,
+                            increment=1, valinit=0)
 
     def update(val):
-        l.set_data(frames[sframe.val])
+        l.set_data(frames[int(sframe.val)])
         plt.draw()
+        plt.show()
 
     sframe.on_changed(update)
     plt.show()
